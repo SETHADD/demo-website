ace96af9b248812c48b3cefe7aa141e055cb4d99
Seth Kwame Addo <addoseth7@gmail.com>
2023-06-08 17:49:00 +0100

    initial commit of the website

---
+++ app.js
@@ -0,0 +1,31 @@
+let myName = "Seth"
+console.log(myName);
+let myPet = "Dog"
+console.log(myPet);
+console.log(typeof myPet);
+let myPetAge = 7
+console.log(myPetAge);
+console.log(typeof myPetAge);
+let myPetDetails = "My pet is " + myPetAge + "years"
+console.log(myPetDetails);
+console.log(typeof myPetDetails);
+let likeWalks = false
+console.log(likeWalks);
+console.log(typeof likeWalks);
+let addition = 7 + 7;
+console.log(addition);
+let susbstitution = 5 - 2;
+console.log(susbstitution);
+let multiplication = 8 * 9;
+console.log(multiplication);
+let division = 4/5;
+console.log(multiplication);
+
+let myNum = 5;
+console.log(myNum);
+let thismyNum = ++myNum;
+console.log(thismyNum);
+
+let hiThere = prompt("Hi there we are here to help, can we know your name ");
+console.log(hiThere);
+
